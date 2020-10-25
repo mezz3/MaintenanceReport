8e3f54972b294ed68b12ee28adbd8a5becad18d5
mezz3 <60070070@kmitl.ac.th>
2020-10-25 20:27:30 +0700

    s410un

--- MaintenanceReport/MaintenanceReport/settings.py
+++ MaintenanceReport/MaintenanceReport/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q!rq)dwm^xc1-q5roa#*lcygg%8vgpmg%ao6rr*f=$n*t8b5t)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
